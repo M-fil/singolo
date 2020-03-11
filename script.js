3f9d5c57a2e3d5b2a00d7735c9ce97f86df61d10
m_fil <filanovich.maxim11b@mail.ru>
2020-03-11 20:42:04 +0300

    feat: create slider functionality

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*Navigation*/
 function selectSingleElementFromList(element) {
     let selected = element.closest('ul').querySelectorAll('.selected');
     for (const item of selected) {
@@ -17,3 +18,57 @@ headerNavigationList.addEventListener('click', (event) => {
     const blockId = document.querySelector(`[data-scrollid=${event.target.getAttribute('href').slice(1)}]`);
     blockId.scrollIntoView({block: 'center', behavior: 'smooth'});
 });
+
+/*Slider*/
+
+const sliderBlock = document.querySelector('.slider');
+const slidesContainer = document.querySelector('.slider-content');
+
+const prevButton = sliderBlock.querySelector('.prev');
+const nextButton = sliderBlock.querySelector('.next');
+
+const firstSlide = `
+<div class="first-slide">
+    <img src="assets/phone1.png" alt="" class="slider-content-item">
+    <img src="assets/phone2.png" alt="" class="slider-content-item">
+</div>`;
+
+const secondSlide = `
+<div class="second-slide">
+    <img src="assets/slider-content-center.png" alt="">
+    <img src="assets/slider-content-left.png" alt="">
+    <img src="assets/slider-content-right.png" alt="">
+</div>`;
+
+const list = [firstSlide, secondSlide];
+let current = 0;
+let length = list.length - 1;
+
+nextButton.parentElement.addEventListener('click', () => {
+    current = current < length ? current + 1 : 0;
+    slidesContainer.innerHTML = list[current]
+
+    /*if (current === 1) {
+        sliderBlock.classList.add('second-slide')
+        sliderBlock.style.backgroundColor = '#648BF0';
+    } else {
+        sliderBlock.classList.remove('second-slide')
+        sliderBlock.style.backgroundColor = '#ea676b';
+    }*/
+});
+
+prevButton.parentElement.addEventListener('click', () => {
+    current = current > 0 ? current - 1 : length;
+    slidesContainer.innerHTML = list[current];
+
+    /*if (current === 1) {
+        sliderBlock.classList.add('second-slide')
+        sliderBlock.style.backgroundColor = '#648BF0';
+    } else {
+        sliderBlock.classList.remove('second-slide')
+        sliderBlock.style.backgroundColor = '#ea676b';
+    }*/
+});
+
+//const firstSlide = slidesContainer.querySelector('.first-slide');
+//const secondSlide = slidesContainer.querySelector('.second-slide');
